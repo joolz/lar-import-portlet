13a74ccc1f085c83055824da23c6db10175a27f2
joolz <joolz@xs4all.nl>
2016-06-17 15:08:29 +0200

    Dummy frontend, need parameters and background task

--- src/main/java/joolz/test/MyBean.java
+++ src/main/java/joolz/test/MyBean.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2016 Open Universiteit Nederland
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see &lt;http://www.gnu.org/licenses/&gt;.
+ */
 package joolz.test;
 
 import com.liferay.faces.portal.context.LiferayFacesContext;
@@ -8,44 +24,49 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringPool;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.model.Group;
 import com.liferay.portal.service.GroupLocalServiceUtil;
 import com.liferay.portal.service.LayoutServiceUtil;
 import com.liferay.portlet.documentlibrary.service.DLFileEntryLocalServiceUtil;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PreDestroy;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
 
 import nl.ou.dlwo.products.model.Product;
 import nl.ou.dlwo.products.service.ProductLocalServiceUtil;
 
 import org.primefaces.event.FileUploadEvent;
-import org.primefaces.model.UploadedFile;
 
+/**
+ * The Class MyBean.
+ */
 @ManagedBean
 @ViewScoped
 public class MyBean implements Serializable {
 	private static final Log LOG = LogFactoryUtil.getLog(MyBean.class);
 	private static final long serialVersionUID = 42L;
-	private UploadedFile uploadedFile;
+	private File uploadedFile = null;
 	private transient Integer maxFileSize = null;
-	private transient Boolean uploadEnabled = null;
-	private transient List<Group> allowedSites = null;
-
-	public UploadedFile getUploadedFile() {
-		return uploadedFile;
-	}
-
-	public void setUploadedFile(UploadedFile uploadedFile) {
-		this.uploadedFile = uploadedFile;
-	}
+	private transient List<SelectItem> sites = null;
+	private String site;
+	private static final String TMPFILE_PREFIX = "dlwo-larimport";
+	private static final String TMPFILE_POSTFIX = ".lar";
 
 	public Integer getMaxFileSize() {
 		if (maxFileSize == null) {
@@ -55,6 +76,18 @@ public class MyBean implements Serializable {
 		return maxFileSize;
 	}
 
+	/**
+	 * Delete temp file entry.
+	 *
+	 * @param groupId
+	 *            the group id
+	 * @param folderName
+	 *            the folder name
+	 * @throws PortalException
+	 *             the portal exception
+	 * @throws SystemException
+	 *             the system exception
+	 */
 	protected void deleteTempFileEntry(long groupId, String folderName) throws PortalException, SystemException {
 		String[] tempFileEntryNames = LayoutServiceUtil.getTempFileEntryNames(groupId, folderName);
 		for (String tempFileEntryName : tempFileEntryNames) {
@@ -62,90 +95,95 @@ public class MyBean implements Serializable {
 		}
 	}
 
+	/**
+	 * Upload listener for a single upload that will be imported right away.
+	 *
+	 * @param event
+	 *            the event
+	 */
 	public void uploadListener(FileUploadEvent event) {
-		uploadedFile = event.getFile();
-		LOG.debug("file is " + uploadedFile.getFileName());
+		LOG.debug("file is " + event.getFile().getFileName());
 
-		LiferayFacesContext lfc = LiferayFacesContext.getInstance();
-
+		InputStream inputStream = null;
+		OutputStream outputStream = null;
 		try {
+			inputStream = event.getFile().getInputstream();
+			uploadedFile = File.createTempFile(TMPFILE_PREFIX, TMPFILE_POSTFIX);
+			outputStream = new FileOutputStream(uploadedFile);
 
-			deleteTempFileEntry(lfc.getScopeGroupId(), ExportImportHelper.TEMP_FOLDER_NAME);
+			int read = 0;
+			byte[] bytes = new byte[1024];
 
-			FileEntry fileEntry = LayoutServiceUtil.addTempFileEntry(lfc.getScopeGroupId(), uploadedFile.getFileName(),
-					ExportImportHelper.TEMP_FOLDER_NAME, uploadedFile.getInputstream(), uploadedFile.getContentType());
-
-			InputStream inputStream = DLFileEntryLocalServiceUtil.getFileAsStream(fileEntry.getFileEntryId(),
-					fileEntry.getVersion(), false);
-
-			Map<String, String[]> parameterMap = null;
-
-			LayoutServiceUtil.importLayoutsInBackground(fileEntry.getTitle(), lfc.getScopeGroupId(), false,
-					parameterMap, inputStream);
-
-			deleteTempFileEntry(lfc.getScopeGroupId(), ExportImportHelper.TEMP_FOLDER_NAME);
-
-			lfc.addGlobalSuccessInfoMessage();
-
-		} catch (Exception e) {
+			while ((read = inputStream.read(bytes)) != -1) {
+				outputStream.write(bytes, 0, read);
+			}
+		} catch (IOException e) {
 			LOG.error(e);
-			lfc.addGlobalUnexpectedErrorMessage();
-		}
-	}
-
-	public boolean isUploadEnabled() {
-		if (uploadEnabled == null) {
-			getAllowedSites(); // TODO temporary here
-			LOG.debug("Determine uploadEnabled");
-			uploadEnabled = false;
+		} finally {
 			try {
-				LiferayFacesContext lfc = LiferayFacesContext.getInstance();
-				Product product = ProductLocalServiceUtil.getProductByG(lfc.getScopeGroupId());
-				if (product != null) {
-					LOG.debug("Found product " + product.getName());
-					int state = product.getState();
-					LOG.debug("has state " + state);
-					if (WorkflowConstants.STATUS_DRAFT == state) {
-						LOG.debug("product is draft, set uploadEnabled to true");
-						uploadEnabled = true;
-					}
-				}
-			} catch (SystemException e) {
+				inputStream.close();
+			} catch (IOException e) {
 				LOG.error(e);
 			}
 		}
-		return uploadEnabled;
+		if (outputStream != null) {
+			try {
+				outputStream.flush();
+				outputStream.close();
+			} catch (IOException e) {
+				LOG.error(e);
+			}
+		}
+
 	}
 
+	/**
+	 * Checks if at least one site is OK for upload.
+	 *
+	 * @return true, if is upload enabled
+	 */
+	public boolean isUploadEnabled() {
+		return getSites().size() > 0;
+	}
+
+	/**
+	 * Checks if a site is draft.
+	 *
+	 * @param groupId
+	 *            the group id
+	 * @return true, if is site draft
+	 */
 	private boolean isSiteDraft(final long groupId) {
 		boolean result = false;
 		try {
-			LiferayFacesContext lfc = LiferayFacesContext.getInstance();
-			LOG.debug("Total number of products " + ProductLocalServiceUtil.getProductsCount());
 			Product product = ProductLocalServiceUtil.getProductByG(groupId);
 			if (product != null) {
-				LOG.debug("Found product " + product.getName() + " for site "
-						+ GroupLocalServiceUtil.getGroup(groupId).getName());
+				LOG.debug("Found product " + product.getName());
 				int state = product.getState();
-				LOG.debug("has state " + state);
 				if (WorkflowConstants.STATUS_DRAFT == state) {
-					LOG.debug("product is draft, set uploadEnabled to true");
+					LOG.debug("Site " + product.getGroupId() + " has product " + product.getProductId()
+							+ " that is draft");
 					result = true;
 				}
 			}
-		} catch (SystemException | PortalException e) {
+		} catch (SystemException e) {
 			LOG.error(e);
 		}
 		return result;
 	}
 
-	public List<Group> getAllowedSites() {
-		if (allowedSites == null) {
-			allowedSites = new ArrayList<Group>();
+	/**
+	 * Gets a list of sites that are OK for import.
+	 *
+	 * @return the allowed sites
+	 */
+	public List<SelectItem> getSites() {
+		if (sites == null) {
+			sites = new ArrayList<SelectItem>();
 			try {
 				for (Group group : GroupLocalServiceUtil.getGroups(-1, -1)) {
 					if (group.isSite() && isSiteDraft(group.getGroupId())) {
-						LOG.debug("found group " + group.getName());
+						sites.add(new SelectItem(group.getGroupId(), group.getName()));
 					}
 				}
 			} catch (SystemException e) {
@@ -153,6 +191,73 @@ public class MyBean implements Serializable {
 			}
 
 		}
-		return allowedSites;
+		return sites;
+	}
+
+	public void setSite(String site) {
+		this.site = site;
+	}
+
+	public String getSite() {
+		return site;
+	}
+
+	@PreDestroy
+	public void cleanup() {
+		if (uploadedFile.exists()) {
+			uploadedFile.delete();
+		}
+	}
+
+	/**
+	 * Do actual import.
+	 *
+	 * @return the string
+	 */
+	public String doImport() {
+		LiferayFacesContext lfc = LiferayFacesContext.getInstance();
+		if (getSite() == null || getSite().trim().isEmpty()) {
+			LOG.debug("No site selected");
+			lfc.addGlobalErrorMessage("No site selected");
+		} else if (uploadedFile == null) {
+			LOG.debug("No file uploaded");
+			lfc.addGlobalErrorMessage("No file uploaded");
+		} else {
+
+			long groupId = Long.valueOf(getSite());
+			try {
+
+				deleteTempFileEntry(groupId, ExportImportHelper.TEMP_FOLDER_NAME);
+
+				FileEntry fileEntry = LayoutServiceUtil.addTempFileEntry(groupId, uploadedFile.getName(),
+						ExportImportHelper.TEMP_FOLDER_NAME, new FileInputStream(uploadedFile),
+						URLConnection.guessContentTypeFromName(uploadedFile.getName()));
+
+				InputStream inputStream = DLFileEntryLocalServiceUtil.getFileAsStream(fileEntry.getFileEntryId(),
+						fileEntry.getVersion(), false);
+
+				// TODO set parameters
+				Map<String, String[]> parameterMap = null;
+
+				// TODO BackgroundTaskMessageListener:133] Unable to execute
+				// background task
+				LayoutServiceUtil.importLayoutsInBackground(fileEntry.getTitle(), groupId, false, parameterMap,
+						inputStream);
+
+				lfc.addGlobalSuccessInfoMessage();
+
+			} catch (Exception e) {
+				LOG.error(e);
+				lfc.addGlobalUnexpectedErrorMessage();
+			} finally {
+				try {
+					deleteTempFileEntry(groupId, ExportImportHelper.TEMP_FOLDER_NAME);
+					uploadedFile.delete();
+				} catch (PortalException | SystemException e) {
+					LOG.error(e.getMessage());
+				}
+			}
+		}
+		return StringPool.BLANK;
 	}
 }
